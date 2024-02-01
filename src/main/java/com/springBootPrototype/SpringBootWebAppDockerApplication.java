fb8db987cb1992402e3a4e916885cd40473ead6f
mhkaaniche <mhkaaniche@gmail.com>
2024-02-01 22:20:22 +0100

    SpringBoot_Prototype - repo to clone

---
+++ src/main/java/com/springBootPrototype/SpringBootWebAppDockerApplication.java
@@ -0,0 +1,13 @@
+package com.springBootPrototype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebAppDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebAppDockerApplication.class, args);
+	}
+
+}
